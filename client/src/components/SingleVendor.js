773bd7e04c29d590dcf46c55f651a6257fd33f81
mfhan <mf212mf@gmail.com>
2019-09-15 23:06:21 -0400

    tweak CTA in SingleVendor

--- client/src/components/SingleVendor.js
+++ client/src/components/SingleVendor.js
@@ -5,7 +5,7 @@ const SingleVendor = (props) => {
     <div className="single-vendor">
       <h3>{props.vendor.username}</h3>
       <p>{props.vendor.snapshot}</p>
-      <p>Donate to their website: {props.vendor.website}.</p>
+      <p>Visit their website: {props.vendor.website}.</p>
       <button onClick={() => props.showUpdateForm(props.vendor.id)}>EDIT</button>
       <button onClick={() => props.handleDelete(props.vendor.id)}>DELETE</button>
     </div>
